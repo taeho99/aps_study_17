8ecbb903208c302f6a556d0ed290ba4adee01a1d
임태호 <rktlrhrl531@naver.com>
2024-08-01 17:43:48 +0900

    [240801] 임태호

--- SWEA_4613_러시아 국기 같은 깃발/S4613_임태호.java
+++ SWEA_4613_러시아 국기 같은 깃발/S4613_임태호.java
@@ -21,15 +21,20 @@ public class S4613_임태호 {
         	rowSize = Integer.parseInt(st.nextToken());
         	colSize = Integer.parseInt(st.nextToken());
         	
-        	flag = new String[rowSize];
-        	pointer = new int[2];
+        	flag = new String[rowSize]; //입력으로 들어온 국기
+        	pointer = new int[2]; //W, B, R 구분하는 위치 2곳
             visited = new boolean[rowSize-1];
-            result = Integer.MAX_VALUE;
+            result = Integer.MAX_VALUE; //최솟값 탐색 위해 MAX_VALUE로 초기화
             
+            //[입력] 국기
             for(int row=0; row<rowSize; row++) {
             	flag[row] = br.readLine();
             }
             
+            //W, B, R을 구분하는 위치를 구하기 위해 DFS 탐색
+            //예를 들어, rowSize == 4 이면 W, B, R을 구분하는 위치는
+            //[1,2) [2,3) [1,3)
+            //중복없는 조합 (rowSize-1)C2 -> 3C2
             dfs(rowSize-1, 2, 0, 0);
             sb.append(result).append('\n');
         }
@@ -57,6 +62,7 @@ public class S4613_임태호 {
 		int diffCnt = 0;
 		
 		//white matching
+		//[0, pointer[0]) 위치는 W이어야 함. W가 아닌 갯수 카운팅
 		for(int row=0; row<pointer[0]; row++) {
 			for(char ch : flag[row].toCharArray()) {
 				if(ch != 'W') diffCnt++;
@@ -64,6 +70,7 @@ public class S4613_임태호 {
 		}
 		
 		//blue matching
+		//[pointer[0], pointer[1]) 위치는 B이어야 함. B가 아닌 갯수 카운팅
 		for(int row=pointer[0]; row<pointer[1]; row++) {
 			for(char ch : flag[row].toCharArray()) {
 				if(ch != 'B') diffCnt++;
@@ -71,6 +78,7 @@ public class S4613_임태호 {
 		}
 		
 		//red matching
+		//[pointer[1], rowSize) 위치는 R이어야 함. R이 아닌 갯수 카운팅
 		for(int row=pointer[1]; row<rowSize; row++) {
 			for(char ch : flag[row].toCharArray()) {
 				if(ch != 'R') diffCnt++;
